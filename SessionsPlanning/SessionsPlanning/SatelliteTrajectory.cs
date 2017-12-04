c1e8fd83f14238ffc449bdbc54a50618f52df871
asapelkin <asapelkin@sa2systems.ru>
2017-12-04 13:03:41 +0300

    Updated lane creation

--- SessionsPlanning/SessionsPlanning/SatelliteTrajectory.cs
+++ SessionsPlanning/SessionsPlanning/SatelliteTrajectory.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using SatelliteRequests;
-using Astronomy;
-using Common;
 using System.Windows.Media.Media3D;
 using System.Spatial;
+
+
+using SatelliteRequests;
+using Common;
+using Astronomy;
 
 namespace SatelliteTrajectory
 {
@@ -16,43 +18,44 @@ namespace SatelliteTrajectory
         public DateTime startDateTime { get; set; }
 
         /// <summary>
-        /// Get strip shooting by trajectory and max roll angle  
+        /// Get shooting lane by trajectory and max roll angle  
         /// </summary>
-        /// <param name="latitude">roll angle.</param>          
-        public List<Tuple<GeoPoint, GeoPoint>> getTrajectoryStrip(double angle)
+        /// <param name="latitude">roll angle in radians.</param>          
+        public List<Tuple<GeoPoint, GeoPoint>> getTrajectoryLane(double angle)
         {
-            double halfWidth = 3;  ///  TODO @todo get halfWidth by angle
-            
-            List<Tuple<GeoPoint, GeoPoint>> strip = new List<Tuple<GeoPoint, GeoPoint>>();
+            List<Tuple<GeoPoint, GeoPoint>> lane = new List<Tuple<GeoPoint, GeoPoint>>();
             
             for (int i = 0; i < points.Count - 1; i++)
             {
-                GeoPoint point = GeoPoint.FromCartesian(points[i].Position.X, points[i].Position.Y, points[i].Position.Z);
-                GeoPoint nextPoint = GeoPoint.FromCartesian(points[i+1].Position.X, points[i+1].Position.Y, points[i+1].Position.Z);
+                Vector3D vectToCent = new Vector3D(points[i].Position.X, points[i].Position.Y, points[i].Position.Z);
+                double altitude = vectToCent.Length - Constants.EarthRadius;
 
-                double x = nextPoint.Latitude - point.Latitude;
-                double y = nextPoint.Longitude - point.Longitude;
-                Vector3D dirVect = new Vector3D(x,y,0);
-                Vector3D axis = new Vector3D(0, 0, 1);     
-           
-                Matrix3D leftRotMatr = Matrix3D.Identity;
-                leftRotMatr.Rotate(new Quaternion(axis, -90));
-                Vector3D leftVect = leftRotMatr.Transform(dirVect);
+                double halfWidth = altitude * Math.Tan(angle);  /// halfWidth of the lane 
+
+                GeoPoint point = AstronomyMath.GreenwichToSpherical(points[i].Position); 
+                GeoPoint nextPoint = AstronomyMath.GreenwichToSpherical(points[i].Position); 
+
+                double dirx = nextPoint.Latitude - point.Latitude;
+                double diry = nextPoint.Longitude - point.Longitude;
+                Vector3D dirVect = new Vector3D(dirx, diry, 0);
+
+                RotateTransform3D leftTransform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), -90));
+                RotateTransform3D rightTransform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), 90));
+                
+                Vector3D leftVect = leftTransform.Transform(dirVect);
                 leftVect.Normalize();
                 leftVect = leftVect * halfWidth;
 
-                Matrix3D rightRotMatr = Matrix3D.Identity;
-                rightRotMatr.Rotate(new Quaternion(axis, 90));
-                Vector3D rightVect = rightRotMatr.Transform(dirVect);
+                Vector3D rightVect = rightTransform.Transform(dirVect);
                 rightVect.Normalize();
                 rightVect = rightVect * halfWidth;
 
                 GeoPoint leftPoint = new GeoPoint(point.Latitude + leftVect.X, point.Longitude + leftVect.Y, false);
                 GeoPoint rightPoint = new GeoPoint(point.Latitude + rightVect.X, point.Longitude + rightVect.Y, false);
 
-                strip.Add(new Tuple<GeoPoint, GeoPoint>(leftPoint, rightPoint));
-            }                       
-            return strip;
+                lane.Add(new Tuple<GeoPoint, GeoPoint>(leftPoint, rightPoint));
+            }
+            return lane;
         }
     }
 
