dbafe7790a1434244273b7deef5100648c4b4bf1
asapelkin <asapelkin@sa2systems.ru>
2017-12-13 13:35:24 +0300

    Added function prototypes

--- SessionsPlanning/SessionsPlanning/SatelliteSessions.cs
+++ SessionsPlanning/SessionsPlanning/SatelliteSessions.cs
@@ -7,4 +7,25 @@ using System.Threading.Tasks;
 
 namespace SatelliteSessions
 {
+    public class Sessions 
+    {
+        public static bool isRequestFeasible(DateTime dateFrom, DateTime dateTo, int Max_SOEN_anlge, double minCoverPerc,
+                                             int Max_sun_angle, int Min_sun_angle, string wktPolygon)
+        {
+            return true;
+        }
+
+        public static IList<CaptureConf> getCaptureConfArray(int priority, DateTime dateFrom, DateTime dateTo, int Max_SOEN_anlge,
+                                                             double minCoverPerc, int Max_sun_angle, int Min_sun_angle, string wktPolygon)
+        {            
+            return new List<CaptureConf>();
+        }
+
+        public struct CaptureConf
+        {
+            public DateTime dateFrom;
+            public DateTime dateTo;
+            public double rollAngle; 
+        }
+    }    
 }
