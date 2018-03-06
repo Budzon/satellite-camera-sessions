3f3397dad558571593f6260a414b21afe04a5a03
Stanislav Budzinskiy <budzon@yandex.ru>
2018-03-06 13:24:21 +0300

    orbit height constant

--- SessionsPlanning/SessionsPlanning/OptimalChain/Constants.cs
+++ SessionsPlanning/SessionsPlanning/OptimalChain/Constants.cs
@@ -22,5 +22,6 @@ namespace OptimalChain
         public const double earthRotSpeed = Math.PI / (12 * 60 * 60);
         public const int pitchStep = 1; // в градусах
         public const double orbital_inclination = 1.7104; // угл наклона орбиты в градусах
+        public const double orbit_height = 650; // км (или 720?)
     }
 }
